395b0c35bef05e46424ff251c3c5aa6657a5a6de
AhmedTahasubs <ahmedtahaamuhammed@gmail.com>
2025-02-12 17:27:28 +0200

    new Project

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -4,16 +4,23 @@ import ProjectCard from './ProjectCard';
 export default function Projects() {
   const works = [
     {
+      imgSrc: '/images/social.png',
+      title: 'Social Media App',
+      tags: ['Next.Js','TypeScript', 'Responsive','Tailwind','Material UI','API','Axios','Formik','Flowbite','YUP'],
+      githubLink: 'https://github.com/AhmedTahasubs/Next.js-Social-App',
+      demoLink: 'https://next-js-social-app-nine.vercel.app/',
+    },
+    {
       imgSrc: '/images/Admin.png',
       title: 'Admin Dashboard App',
-      tags: ['React', 'Responsive','Tailwind','Framer-Motion','Lucide-React','React-Router','Recharts'],
+      tags: ['React.Js', 'JavaScript','Responsive','Tailwind','Framer-Motion','Lucide-React','React-Router','Recharts'],
       githubLink: 'https://github.com/AhmedTahasubs/Admin-Dashboard',
       demoLink: 'https://admin-dashboard-ten-ochre.vercel.app/',
     },
     {
       imgSrc: '/images/Fresh-Cart.png',
       title: 'Fresh-Cart E-commerce app ',
-      tags: ['React', 'API', 'Responsive','Tailwind','Context','Axios','Formik','YUP','Dark & Light mode'],
+      tags: ['React.Js','JavaScript', 'API', 'Responsive','Tailwind','Context','Axios','Formik','YUP','Dark & Light mode'],
       githubLink: 'https://github.com/AhmedTahasubs/Fresh-Cart.git',
       demoLink: 'https://fresh-cart-tawny-phi.vercel.app/',
     },
@@ -34,7 +41,7 @@ export default function Projects() {
     {
       imgSrc: '/images/games.png',
       title: 'Game Reviews',
-      tags: ['HTML', 'CSS','JavaScript','API','Responsive','Bootstrap'],
+      tags: ['HTML', 'CSS','JavaScript','OOP','API','Responsive','Bootstrap'],
       githubLink: 'https://github.com/AhmedTahasubs/GameReviews',
       demoLink: 'https://ahmedtahasubs.github.io/GameReviews/',
     },
